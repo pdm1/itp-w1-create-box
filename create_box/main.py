063741f739ec8347f93e70d709c5bb4138935d8c
Patrick Matthies <coding.newb80@gmail.com>
2017-01-14 14:59:01 +0000

    Final solution

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,36 @@
 
 
 def create_box(height, width, character):
-    pass
+  a_str = ""
+  if height is 0 or width is 0:
+    return "Either height or width is incorrect"
+  for row in range(height):
+    for col in range(width):
+      a_str += character
+    a_str += "\n"
+  print(a_str)
+  return a_str
+  
+def create_outer_box(height,width,char="!"):
+  a_str = ""
+  if height is 0 or width is 0:
+    return "Either height or width is incorrect"
+  if width is 1 and height > 1:
+      a_str = (char + "\n")*height
+      print(a_str)
+      return a_str
+  else:
+    for row in range(height):
+      for col in range(1):
+        if row is 0 or row is height - 1:
+          a_str += char*width + "\n"
+        elif col > 0 or col < width - 1:
+          a_str += str(char + " " * (width - 2) + char + "\n") 
+    print(a_str)
+    return a_str
+  
 
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+    create_outer_box(6,9)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import unittest
 
-from create_box import create_box
+from create_box import create_box,create_outer_box
+
 
 first_box_expected = """
 ****
@@ -18,6 +19,14 @@ xxxxxxxxxxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxxxxxxxxxxx
 """.lstrip()
 
+fourth_box_expected = """
+!!!!!!!!!
+!       !
+!       !
+!       !
+!       !
+!!!!!!!!!
+""".lstrip()
 
 class TestCreateBox(unittest.TestCase):
     def test_box(self):
@@ -25,5 +34,11 @@ class TestCreateBox(unittest.TestCase):
 
     def test_small_box(self):
         self.assertEqual(create_box(1, 1, '@'), second_box_expected)
+        
+    def test_middle_box(self):
+        self.assertEqual(create_box(3,24, 'x'), third_box_expected)
+        
+    def test_outer_box(self):
+        self.assertEqual(create_outer_box(6,9,'!'), fourth_box_expected)
 
     # Add your own test using third_box_expected
